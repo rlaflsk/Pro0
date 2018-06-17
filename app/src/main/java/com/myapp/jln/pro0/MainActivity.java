53af09bb646a3d5d472a5f12629bd47b989da0c8
rlaflsk <jln612_5@126.com>
2018-06-17 12:48:43 +0800

    delete all
    (cherry picked from commit ed7e0a5)

--- app/src/main/java/com/myapp/jln/pro0/MainActivity.java
+++ app/src/main/java/com/myapp/jln/pro0/MainActivity.java
@@ -30,7 +30,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonReader.setOnClickListener(this);
         buttonNews.setOnClickListener(this);
         buttonGraduate.setOnClickListener(this);
-        
+
     }
 
     @Override
