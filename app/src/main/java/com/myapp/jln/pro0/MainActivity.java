5bf2e5d3e033906f8bed633239ab9bb45d4759b6
rlaflsk <jln612_5@126.com>
2018-06-17 13:18:06 +0800

    comm1

--- app/src/main/java/com/myapp/jln/pro0/MainActivity.java
+++ app/src/main/java/com/myapp/jln/pro0/MainActivity.java
@@ -30,7 +30,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonReader.setOnClickListener(this);
         buttonNews.setOnClickListener(this);
         buttonGraduate.setOnClickListener(this);
+
+        Log.d("###", "comm1");
         
+
     }
 
     @Override
