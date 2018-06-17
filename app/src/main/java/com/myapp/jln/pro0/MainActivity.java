7fe622ad99bb4bef49c2919d421e0049f718b0a5
rlaflsk <jln612_5@126.com>
2018-06-17 13:22:41 +0800

    Revert "comm1"
    This reverts commit 5bf2e5d

--- app/src/main/java/com/myapp/jln/pro0/MainActivity.java
+++ app/src/main/java/com/myapp/jln/pro0/MainActivity.java
@@ -30,10 +30,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonReader.setOnClickListener(this);
         buttonNews.setOnClickListener(this);
         buttonGraduate.setOnClickListener(this);
-
-        Log.d("###", "comm1");
-
-
     }
 
     @Override
