c006d552c499010c10a2a3fc6a8cd483320a7748
rlaflsk <jln612_5@126.com>
2018-06-17 13:27:45 +0800

    commit2

--- app/src/main/java/com/myapp/jln/pro0/MainActivity.java
+++ app/src/main/java/com/myapp/jln/pro0/MainActivity.java
@@ -32,6 +32,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonGraduate.setOnClickListener(this);
 
         Log.d("###", "commit1");
+
+        Log.d("###", "commit2");
+        
     }
 
     @Override
