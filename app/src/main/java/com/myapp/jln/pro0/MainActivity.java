5d0ca4db1a5a4a9410c53cb8c0b26d588145021a
rlaflsk <jln612_5@126.com>
2018-06-22 06:31:18 +0800

    33333

--- app/src/main/java/com/myapp/jln/pro0/MainActivity.java
+++ app/src/main/java/com/myapp/jln/pro0/MainActivity.java
@@ -18,7 +18,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //
 
         buttonMovie = (Button)findViewById(R.id.buttonMovie);
         buttonShare = (Button)findViewById(R.id.buttonShare);
@@ -32,9 +31,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         buttonNews.setOnClickListener(this);
         buttonGraduate.setOnClickListener(this);
 
-//
-
-        //
     }
 
     @Override
